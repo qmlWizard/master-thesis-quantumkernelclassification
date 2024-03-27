16adee5652d0ac203acb3331e74ec8dd732b933a
da02345 <digvijaysinh.ajarekar@stud.th-deg.de>
2024-03-27 10:33:32 +0100

    Initial commit

---
+++ Code/trainingQuantumKernels.py
@@ -0,0 +1,262 @@
+"""
+Tutorial: https://pennylane.ai/qml/demos/tutorial_kernel_based_training/
+"""
+
+
+from pennylane import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pennylane as qml
+import pandas as pd
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+
+
+def _make_circular_data(num_sectors):
+    """Generate datapoints arranged in an even circle."""
+    center_indices = np.array(range(0, num_sectors))
+    sector_angle = 2 * np.pi / num_sectors
+    angles = (center_indices + 0.5) * sector_angle
+    x = 0.7 * np.cos(angles)
+    y = 0.7 * np.sin(angles)
+    labels = 2 * np.remainder(np.floor_divide(angles, sector_angle), 2) - 1
+
+    return x, y, labels
+
+
+def make_double_cake_data(num_sectors):
+    x1, y1, labels1 = _make_circular_data(num_sectors)
+    x2, y2, labels2 = _make_circular_data(num_sectors)
+
+    # x and y coordinates of the datapoints
+    x = np.hstack([x1, 0.5 * x2])
+    y = np.hstack([y1, 0.5 * y2])
+
+    # Canonical form of dataset
+    X = np.vstack([x, y]).T
+
+    labels = np.hstack([labels1, -1 * labels2])
+
+    # Canonical form of labels
+    Y = labels.astype(int)
+
+    return X, Y
+
+
+def plot_double_cake_data(X, Y, ax, num_sectors=None):
+    """Plot double cake data and corresponding sectors."""
+    x, y = X.T
+    cmap = mpl.colors.ListedColormap(["#FF0000", "#0000FF"])
+    ax.scatter(x, y, c=Y, cmap=cmap, s=25, marker="s")
+
+    if num_sectors is not None:
+        sector_angle = 360 / num_sectors
+        for i in range(num_sectors):
+            color = ["#FF0000", "#0000FF"][(i % 2)]
+            other_color = ["#FF0000", "#0000FF"][((i + 1) % 2)]
+            ax.add_artist(
+                mpl.patches.Wedge(
+                    (0, 0),
+                    1,
+                    i * sector_angle,
+                    (i + 1) * sector_angle,
+                    lw=0,
+                    color=color,
+                    alpha=0.1,
+                    width=0.5,
+                )
+            )
+            ax.add_artist(
+                mpl.patches.Wedge(
+                    (0, 0),
+                    0.5,
+                    i * sector_angle,
+                    (i + 1) * sector_angle,
+                    lw=0,
+                    color=other_color,
+                    alpha=0.1,
+                )
+            )
+            ax.set_xlim(-1, 1)
+
+    ax.set_ylim(-1, 1)
+    ax.set_aspect("equal")
+    ax.axis("off")
+
+    return ax
+
+def powerFaultData():
+
+    powerData = pd.read_csv('Testdata.csv')
+    
+    X = []
+    Y = []
+
+    for i in powerData.iterrows():
+        X.append([i[1][p] for p in range(0, len(i[1]))])
+        Y.append(int(i[1][6]))
+
+    Y = np.asarray(Y).astype(int)
+
+    xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size = 0.05, shuffle=True, random_state=42)
+    xTrain = preprocessing.normalize(xTrain)
+    xTest = preprocessing.normalize(xTest)
+
+    return xTrain, yTrain
+          
+def target_alignment(
+    X,
+    Y,
+    kernel,
+    assume_normalized_kernel=False,
+    rescale_class_labels=True,
+):
+    """Kernel-target alignment between kernel and labels."""
+
+    K = qml.kernels.square_kernel_matrix(
+        X,
+        kernel,
+        assume_normalized_kernel=assume_normalized_kernel,
+    )
+
+    if rescale_class_labels:
+        nplus = np.count_nonzero(np.array(Y) == 1)
+        nminus = len(Y) - nplus
+        _Y = np.array([y / nplus if y == 1 else y / nminus for y in Y])
+    else:
+        _Y = np.array(Y)
+
+    T = np.outer(_Y, _Y)
+    inner_product = np.sum(K * T)
+    norm = np.sqrt(np.sum(K * K) * np.sum(T * T))
+    inner_product = inner_product / norm
+
+    return inner_product
+	 
+
+num_sectors = 100
+X, Y = make_double_cake_data(num_sectors)
+ax = plot_double_cake_data(X, Y, plt.gca(), num_sectors=num_sectors)
+
+X, Y = powerFaultData()
+
+print("Data: ", X[0])
+print("Label: ", Y[0])
+
+dev = qml.device("default.qubit", wires=6, shots=None)
+wires = dev.wires.tolist()
+
+
+
+def layer(x, params, wires, i0=0, inc=1):
+    """Building block of the embedding ansatz"""
+    i = i0
+    for j, wire in enumerate(wires):
+        qml.Hadamard(wires=[wire])
+        qml.RZ(x[i % len(x)], wires=[wire])
+        #qml.RZ(x[i], wires=[wire])
+        i += inc
+        qml.RY(params[0, j], wires=[wire])
+
+    qml.broadcast(unitary=qml.CRZ, pattern="ring", wires=wires, parameters=params[1])
+    
+def ansatz(x, params, wires):
+    """The embedding ansatz"""
+    for j, layer_params in enumerate(params):
+        layer(x, layer_params, wires, i0=j * len(wires))
+        
+adjoint_ansatz = qml.adjoint(ansatz)
+
+def random_params(num_wires, num_layers):
+    """Generate random variational parameters in the shape for the ansatz."""
+    return np.random.uniform(0, 2 * np.pi, (num_layers, 2, num_wires), requires_grad=True)
+
+@qml.qnode(dev)
+def kernel_circuit(x1, x2, params):
+    ansatz(x1, params, wires=wires)
+    adjoint_ansatz(x2, params, wires=wires)
+    return qml.probs(wires=wires)
+
+def kernel(x1, x2, params):
+    return kernel_circuit(x1, x2, params)[0]
+
+def accuracy(classifier, X, Y_target):
+    return 1 - np.count_nonzero(classifier.predict(X) - Y_target) / len(Y_target)
+
+
+
+init_params = random_params(num_wires=6, num_layers=6)
+print(init_params)
+kernel_value = kernel(X[0], X[1], init_params)
+print(f"The kernel value between the first and second datapoint is {kernel_value:.3f}")
+print('---------------------------------------------------------------------------------------------')
+print('Quantum Circuit: ')
+drawer = qml.draw(kernel_circuit)
+print(drawer(X[0], X[1], init_params))
+print('---------------------------------------------------------------------------------------------')
+
+
+print("Length of Data Set: ", len(X))
+
+#init_kernel = lambda x1, x2: kernel(x1, x2, init_params)
+#K_init = qml.kernels.square_kernel_matrix(X, init_kernel, assume_normalized_kernel=True)
+
+
+#with np.printoptions(precision=3, suppress=True):
+#    print(K_init)
+    
+
+#svm = SVC(kernel=lambda X1, X2: qml.kernels.kernel_matrix(X1, X2, init_kernel)).fit(X, Y)	
+
+
+
+
+#accuracy_init = accuracy(svm, X, Y)
+#print(f"The accuracy of the kernel with random parameters is {accuracy_init:.3f}")
+
+######
+#kernel target_alignment
+######
+
+print("Kernel Alignment with Gradient Dscent")
+
+params = init_params
+opt = qml.GradientDescentOptimizer(0.2)
+
+for i in range(500):
+    # Choose subset of datapoints to compute the KTA on.
+    
+    subset = np.random.choice(list(range(len(X))), 4)
+
+    #print(type(subset))
+    #print(type(Y))
+    
+    # Define the cost function for optimization
+    cost = lambda _params: -target_alignment(
+        X[subset],
+        Y[subset],
+        lambda x1, x2: kernel(x1, x2, _params),
+        assume_normalized_kernel=True,
+    )
+    
+    # Optimization step
+    params = opt.step(cost, params)
+
+    # Report the alignment on the full dataset every 50 steps.
+    if (i + 1) % 10 == 0:
+        current_alignment = target_alignment(
+            X,
+            Y,
+            lambda x1, x2: kernel(x1, x2, params),
+            assume_normalized_kernel=True,
+        )
+        print(f"Step {i+1} - Alignment = {current_alignment:.3f}")
+
+
+trained_kernel = lambda x1, x2: kernel(x1, x2, params)
+trained_kernel_matrix = lambda X1, X2: qml.kernels.kernel_matrix(X1, X2, trained_kernel)
+svm_trained = SVC(kernel=trained_kernel_matrix).fit(X, Y)
+
+accuracy_trained = accuracy(svm_trained, X, Y)
+print(f"The accuracy of a kernel with trained parameters is {accuracy_trained:.3f}")
